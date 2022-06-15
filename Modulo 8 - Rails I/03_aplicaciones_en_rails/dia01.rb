829cdb8d8d1264a5a03fd9a439bdf67d9565a69a
Rene <reneclavijo@live.com>
2022-06-14 21:09:41 -0400

    agregando el día 2

---
+++ Modulo 8 - Rails I/03_aplicaciones_en_rails/dia01.rb
@@ -0,0 +1,28 @@
+#! Instalar Rails y hablar de su importancia
+
+# gem update --system
+# gem install rails -v 5.2.8
+# rails -v 
+
+# Crear el primer proyecto con comandos:
+#? Crear un proyecto
+# rails new <nombre_proyecto>
+#? Encender el servidor dentro de la carpeta del proyecto
+# rails s
+
+#? Generadores y trabajar con 2 consolas
+# rails g <elemento> opciones
+# Si creamos un controlador
+# rails g controller paginas inicio conoceme
+
+#? Modificar las vistas y observar rutas
+
+#? Comenzar a hablar de ERB
+#* Mostrar el resultado de la operación (valores)
+#* <%= %>
+#* Ejcutar lógica sin mostrar el resultado. (if, each, for, while)
+#* <% %>
+
+#? Layouts y yield
+
+#? Imágenes y CSS
---
+++ Modulo 8 - Rails I/03_aplicaciones_en_rails/dia02.rb
@@ -0,0 +1,43 @@
+#! Versiones para tomar en cuenta en sus instalaciones
+#! WINDOWS
+#* Ruby 2.7.6
+#* Rails 7.0.3
+
+# Lectura del Proyecto
+
+#* Gemfile
+# Lista de dependencias escritas con sus versiones
+# Es una forma de agrupar gemas por entorno
+#? Development
+#? Test
+#? Production
+#-> gem 'faker'
+#! leer el código Gemfie y ejecutar
+#* bundle
+#* bundle install
+
+#* Posible error
+# Address already in use - bind(2) for "127.0.0.1" port 3000 (Errno::EADDRINUSE)
+# rails s -p 3001
+
+
+
+
+
+# Heroku
+# https://devcenter.heroku.com/articles/getting-started-with-ruby 
+# heroku create --stack heroku-18 | para quienes tienen ruby 2.5.3
+
+# Error Failed to install with Bundler
+# bundle lock --add-platform x86_64-linux3.0.
+
+
+
+git add . 
+git commit -m "primer commit"
+git statu
+
+# Plantilla
+# https://html5up.net/phantom 
+
+# Desafío guiado
