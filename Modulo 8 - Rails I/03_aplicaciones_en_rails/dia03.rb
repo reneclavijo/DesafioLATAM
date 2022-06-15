2f457cd66e8f1929886e36c2de34481da3ac1d53
Rene <reneclavijo@live.com>
2022-06-15 18:29:26 -0400

    Co-authored-by: Emerson Espinoza <emersonxinay@users.noreply.github.com> Co-authored-by: German Trigo Cortes <darikHarian@users.noreply.github.com>

---
+++ Modulo 8 - Rails I/03_aplicaciones_en_rails/dia03.rb
@@ -0,0 +1,35 @@
+#! DESAFÍO GUIADO
+
+#* ● Crear un proyecto llamado first_app (1 Punto)
+# Ejecutar desde consola
+# rails new first_app
+# rails new first_app --database=posgresql
+
+#* ● Inicializar Git (1 Punto)
+#? consola
+# cd first_app
+# git add .
+# git commit -m "primer commit"
+
+
+#* ● Añadir los cambios y hacer el primer committ (1 Punto)
+#* ● Crear el controlador pages con las páginas one y two (2 Puntos)
+#* ● Setear la página one como página de inicio (1 Punto)
+#* ● Agregar un título h1 con tu nombre completo en la página de inicio (0.5 Punto)
+#* ● Ejecutar el comando rake routes en la terminal y obtener una captura de pantalla del
+#* resultado (0.5 Punto)
+#* ● Insertar la imagen de la captura de pantalla obtenida a la página two (1 Punto)
+#* ● Añadir los cambios y hacer el segundo commit (0.5 Punto)
+#* ● Agregar el método three al controlador Pages (1 Punto)
+#* ● Agregar un archivo llamado three.html.erb a la carpeta views del controlador pages
+#* (0.5 Punto)
+#* ● En el archivo routes.rb agregar la ruta que apunta al controlador pages con el método
+#* three (1 Punto)
+#* ● Agregar un título h2 y un párrafo Lorem ipsum en la página three (1 Punto)
+#* ● Añadir los cambios y hacer el tercer commit (0.5 Punto)
+#* ● Agregar un navbar al layout (2 Puntos)
+#* ● El navbar debe contener la clase navbar-inverse y su estructura sólo debe contener 3
+#* links donde cada uno llevará a la página correspondiente (2 Puntos)
+#* ● Añadir los cambios y hacer el cuarto commit (1 Punto)
+#* ● Crear un repositorio en GitHub (1 Punto)
+#* ● Inicializar proyecto en Heroku y hacer push (2 Puntos)
